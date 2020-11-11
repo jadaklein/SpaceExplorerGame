bf8a2433d0f8e56c180eb960317f9769bc411719
jadaklein <52722864+jadaklein@users.noreply.github.com>
2020-11-10 16:01:52 -0800

    Add files via upload

---
+++ Space_Explorer_Game/Space_Explorer_Game/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Space_Explorer_Game
+//
+//  Created by Jada Klein on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var listOfWords = ["planet", "supernova", "alien", "astronaut", "cosmic", "asteroid", "galaxy"]
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    @IBOutlet weak var planetImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+        updateUI()
+        
+        // Do any additional setup after loading the view.
+    }
+    var currentGame: Game!
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        
+        planetImageView.image = UIImage(named: "Alien \(currentGame.incorrectMovesRemaining)")
+    }
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+
+}
+
